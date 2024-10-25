d8af2b6466d261ad1d2574e854b68a326e0849bf
yintao <yintao@kuaishou.com>
2024-10-25 15:42:15 +0800

    gitsigns line_blame显示格式

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -1,5 +1,6 @@
 require("gitsigns").setup({
   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_formatter = '<abbrev_sha>, <author>, <author_time:%Y-%m-%d %H:%M:%S> - <summary>',
 })
 
 require("diffview").setup({
