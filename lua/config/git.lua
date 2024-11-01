360bcdacb9c72ca28797b524f1ceea038679bc47
yintao <yintao@kuaishou.com>
2024-11-01 20:58:48 +0800

    add comand to show diff of commit of current line

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -6,3 +6,41 @@ require("gitsigns").setup({
 require("diffview").setup({
   enhanced_diff_hl = true,
 })
+
+
+local gitsigns = require('gitsigns')
+local cache = require('gitsigns.cache').cache
+local log = require('gitsigns.debug.log')
+
+local api = vim.api
+
+vim.api.nvim_create_user_command("OpenCommitInfoOfCurrLine", function()
+    local bufnr = api.nvim_get_current_buf()
+    local bcache = cache[bufnr]
+    if not bcache then
+        log.dprint('Not attached')
+        return
+    end
+    bcache:get_blame()
+
+
+    local blame = assert(bcache.blame)
+
+    -- for i, hl in pairs(blame) do
+    --     local sha = hl.commit.abbrev_sha
+    --     print(i, sha)
+    -- end
+    local blm_win = api.nvim_get_current_win()
+
+    local cursor = unpack(api.nvim_win_get_cursor(blm_win))
+    local cur_sha = blame[cursor].commit.abbrev_sha
+    -- print(cur_sha)
+    local command = string.format("DiffviewOpen %s^!", cur_sha)
+    vim.cmd(command)
+
+    
+end, {})
+
+local map = vim.keymap.set
+-- find the commit of current line, and open all diff view of that commit
+map("n", "<leader>ga", "<cmd>OpenCommitInfoOfCurrLine<cr>", { desc = "diffview of commit of current line" })
