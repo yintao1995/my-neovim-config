91fcd15759ce8f5cfd989dd334c93b520b804abd
yintao <yintao@kuaishou.com>
2024-11-20 14:18:51 +0800

    add vertical scroll bar& status bar

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -1,4 +1,10 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    -- 右侧竖条的状态栏和滚动条,
+    {
+        "lewis6991/satellite.nvim",
+    }
 }
