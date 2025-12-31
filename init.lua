8a68c7e256d5ad5001f08f265b4e00ac417f2ecf
yintao <yintao@kuaishou.com>
2025-12-31 12:03:15 +0800

    1231

--- init.lua
+++ init.lua
@@ -1,21 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.cmd.source(vim.fn.stdpath("config") .. "/vimrc")
 
-
-
-
 -- 禁止通过osc52复制太多内容, 防止删除很多内容时乱码刷屏
 local function copy_to_clipboard(text)
-    if #text > 20000 then
-        print("Skip copy")
-        return
-    end
-    require("vim.ui.clipboard.osc52").copy("+")(text)
+  if #text > 20000 then
+    print("Skip copy")
+    return
+  end
+  require("vim.ui.clipboard.osc52").copy("+")(text)
 end
 local function paste()
   return {
-    vim.split(vim.fn.getreg(''), '\n'),
-    vim.fn.getregtype(''),
+    vim.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
   }
 end
 local platform = vim.loop.os_uname().sysname
@@ -34,7 +31,6 @@ elseif platform == "Darwin" then
   }
 elseif platform == "Win32" then
 end
-
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufReadPost" }, {
   group = vim.api.nvim_create_augroup("SyslogFileType", { clear = true }),
@@ -58,7 +54,6 @@ function _G.delete_lines_with_clipboard_content()
   vim.cmd(command)
   vim.notify(string.format("Deleted lines containing: %s", clipboard_content), vim.log.levels.INFO)
 end
-
 
 require("config.lazy")
 require("config.git")
@@ -85,15 +80,42 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
   end,
 })
 
-vim.g.autoformat = false  -- 关闭nvim-lspconfig默认使能的保持时自动格式化文件
+vim.g.autoformat = false -- 关闭nvim-lspconfig默认使能的保持时自动格式化文件
 vim.o.showtabline = 2
 vim.wo.relativenumber = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter #foldexpr ()"
 vim.opt.foldlevel = 99
-vim.opt.shiftwidth = 4  -- tab设置为4个spaces
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.shiftwidth = 4 -- tab设置为4个spaces
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.g.bigfile_size = 1024 * 1024 * 10 -- 设置大文件为50MB, 超过限制的大文件会自动关闭语法高亮等
+
+-- 1. 定义你想看的符号
+vim.opt.listchars = {
+  space = "·", -- 空格画中间点
+  tab = "› ", -- 制表符画 › + 延伸线
+  trail = "·", -- 行尾空格
+  nbsp = "·",
+}
+-- 默认先不显示
+vim.opt.list = false
+
+-- 2. 自动命令：进入/离开 Visual 模式时切换 list
+local group = vim.api.nvim_create_augroup("ShowWhiteOnVisual", { clear = true })
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*:[vV\x16]*", -- 进入 Visual/块选
+  group = group,
+  callback = function()
+    vim.opt.list = true
+  end,
+})
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "[vV\x16]*:*", -- 离开 Visual/块选
+  group = group,
+  callback = function()
+    vim.opt.list = false
+  end,
+})
 
 -- vim.g.snacks_animate = false -- 默认使能snack动画, 但是搜索时不及时显示匹配序号
 vim.cmd("colorscheme vscode")
