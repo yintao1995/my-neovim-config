75b3158392c4c03dc809d8c48ba537de14522762
yintao <yintao@kuaishou.com>
2026-05-12 20:34:30 +0800

    多terminal保存各自的宽度

---
+++ lua/config/terminal_widths.lua
@@ -0,0 +1,3 @@
+local M = {}
+M.widths = {}
+return M
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,11 +1,21 @@
+local W = require("config.terminal_widths")
+
 return {
   "folke/snacks.nvim",
   opts = {
     terminal = {
       win = {
-        -- 按快键键<c-/>打开floating terminal的时候, 让他保持在right(width=0.3), 而不是bottom(height=0.3)
         position = "right",
-        width = 0.3,
+        width = function(self)
+          local id = vim.b[self.buf].snacks_terminal and vim.b[self.buf].snacks_terminal.id or 1
+          return W.widths[id] or 0.3
+        end,
+        on_close = function(self)
+          if self:win_valid() and self:buf_valid() then
+            local id = vim.b[self.buf].snacks_terminal and vim.b[self.buf].snacks_terminal.id or 1
+            W.widths[id] = vim.api.nvim_win_get_width(self.win)
+          end
+        end,
       },
     },
   },
