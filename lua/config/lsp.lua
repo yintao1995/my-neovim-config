803f8580a7f9572522a4e5bda9c4bb8214b6ea7e
yintao <yintao@kuaishou.com>
2026-05-19 20:32:04 +0800

    取消code action位于行号左侧的灯泡, 从而不会导致窗口跳动; 同时恢复gitsigns插件显示变动的功能

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,2 +1,3 @@
 local map = vim.keymap.set
 map("n", "<leader>go", "<cmd>Lspsaga outline<cr>", { desc = "lspsage: open outline('o' to jump)" })
+map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", { desc = "code action" })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,11 +2,16 @@ return {
   {
     "nvimdev/lspsaga.nvim",
     config = function()
-      require("lspsaga").setup({})
+      require("lspsaga").setup({
+        lightbulb = {
+          sign = false,
+          virtual_text = true,
+        },
+      })
     end,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter", -- optional
-      "nvim-tree/nvim-web-devicons", -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
     },
   },
 }
